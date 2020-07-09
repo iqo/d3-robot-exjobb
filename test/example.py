c2890924c32ad6495f02043acc3e34d05c5ce639
iqo <niklas.brynolf@gmail.com>
2020-07-09 15:21:37 +0200

    test grid

--- test/example.py
+++ test/example.py
@@ -6,10 +6,13 @@ d3 = double.DRDoubleSDK()
 try:
     d3.sendCommand('events.subscribe', { 'events': [
         'DRBase.status',
-        'DRCamera.enable'
+        'DRCamera.enable',
+        'DRGridManager.robotGrid'
+
     ]})
     d3.sendCommand('screensaver.nudge');
     d3.sendCommand('camera.enable', { 'template': 'screen' });
+    d3.sendCommand('gridManager.enable');
     d3.sendCommand('base.requestStatus');
     while True:
         packet = d3.recv()
@@ -17,11 +20,14 @@ try:
             event = packet['class'] + '.' + packet['key']
             if event == 'DRBase.status':
                 print(packet['data'])
+            if event == 'DRGridManager.robotGrid':
+                print('grid enabled')
             elif event == 'DRCamera.enable':
                 print('camera enabled')
 
 except KeyboardInterrupt:
     d3.sendCommand('camera.disable');
+    d3.sendCommand('gridManager.disable')
     d3.sendCommand('screensaver.nudge');
     d3.close()
     print('cleaned up')
