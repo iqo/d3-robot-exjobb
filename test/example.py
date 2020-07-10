faf1f1ce53e77f600464987060f5a21c3616a4dd
iqo <niklas.brynolf@gmail.com>
2020-07-10 16:34:36 +0200

    added navigate and hittest

--- test/example.py
+++ test/example.py
@@ -6,11 +6,18 @@ d3 = double.DRDoubleSDK()
 try:
     d3.sendCommand('events.subscribe', { 'events': [
         'DRBase.status',
-        'DRCamera.enable'
+        'DRCamera.enable',
+        'DRCamera.hitResult'
 
     ]})
     d3.sendCommand('screensaver.nudge');
-    d3.sendCommand('camera.enable', { 'template': 'screen' });
+    # d3.sendCommand('camera.enable', { 'template': 'screen' });
+    d3.sendCommand('camera.enable', {'width':'1152','height':'720','template':'h264ForWebRTC','gstreamer':'appsrc name=d3src ! autovideosink'});
+    #d3.sendCommand('navigate.enable');
+    #d3.sendCommand('navigate.obstacleAvoidance.setLevel',{'level' : '2'});
+    #d3.sendCommand('navigate.target',{'x':'0','y':'0','angleRadians':'0','relative':'true','dock':'false','dockId':'0'});
+    # d3.sendCommand('navigate.hitResult ',{'hit':'true','x':'0.0','y':'0.0','z':'0.0','...'});
+    #d3.sendCommand('camera.hitTest', {'x':'0.5','y':'0.5','highlight':'true'});
     d3.sendCommand('base.requestStatus');
     while True:
         packet = d3.recv()
@@ -20,10 +27,15 @@ try:
                 print(packet['data'])
             elif event == 'DRCamera.enable':
                 print('camera enabled')
+            elif event == 'DRCamera.hitResult':
+                print('hitResult =', packet['data'])
+                
 
 except KeyboardInterrupt:
     d3.sendCommand('camera.disable');
+    d3.sendCommand('navigate.disable');
     d3.sendCommand('screensaver.nudge');
     d3.close()
     print('cleaned up')
     sys.exit(0)
+
