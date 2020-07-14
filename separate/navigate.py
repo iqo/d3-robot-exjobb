a2c90baddc8788349855edd1014c444d1b60aa26
iqo <niklas.brynolf@gmail.com>
2020-07-14 08:00:33 -0700

    added navigation whit event data

--- separate/navigate.py
+++ separate/navigate.py
@@ -9,12 +9,13 @@ def navigate():
         ]})
         d3.sendCommand('navigate.enable');
         d3.sendCommand('navigate.obstacleAvoidance.setLevel',{'level' : '2'});
-        d3.sendCommand('camera.hitTest', {	'hit': true, 'x': 3,'y': 1,'z': 0})
+        d3.sendCommand('camera.hitTest', {'hit': 'true', 'x': 0.5,'y': 0.5,'z': 0, 'highlight': 'true'})
         while True:
             packet = d3.recv()
             if packet != None:
                 event = packet['class'] + '.' + packet['key']
                 if event == 'DRCamera.hitResult':
+                    d3.sendCommand('navigate.target',packet['data'])
                     print('camerahitResult = ---->', packet['data'], '<----')
     except KeyboardInterrupt:
         d3.close()
