6adeeff842cf72ad2431d0160beb46949a91abc4
iqo <niklas.brynolf@gmail.com>
2020-07-14 10:34:55 +0200

    separetd event and commands in spearate function

---
+++ separate/commands.py
@@ -0,0 +1,24 @@
+import ../SDK/double
+import sys
+
+def sendCommands():
+    try:
+        d3 = double.DRDoubleSDK()
+        d3.sendCommand('screensaver.nudge');
+        d3.sendCommand('camera.enable', { 'template': 'screen' });
+        # d3.sendCommand('camera.enable', {'width':1152,'height':720,'template':'h264ForWebRTC','gstreamer':'appsrc name=d3src ! autovideosink'});
+        d3.sendCommand('navigate.enable');
+        d3.sendCommand('navigate.obstacleAvoidance.setLevel',{'level' : '2'});
+        # d3.sendCommand('navigate.target',{'x':'0','y':'0','angleRadians':'0','relative':'true','dock':'false','dockId':'0'});
+        # d3.sendCommand('navigate.hitResult ',{'hit':'true','x':'0.0','y':'0.0','z':'0.0','...'});
+        d3.sendCommand('camera.hitTest', {'x':'0.5','y':'0.5','highlight':'true', 'world': 'True'});
+        # d3.sendCommand('base.requestStatus');
+
+    except KeyboardInterrupt:
+        d3.sendCommand('camera.disable');
+        d3.sendCommand('navigate.disable');
+        d3.sendCommand('screensaver.nudge');
+        d3.close()
+        print('cleaned up')
+        sys.exit(0)
+
---
+++ separate/evets.py
@@ -0,0 +1,37 @@
+import ../SDK/double
+import sys
+
+
+
+def subscribeEvents():
+    d3 = double.DRDoubleSDK()
+    try:
+        d3.sendCommand('events.subscribe', { 'events': [
+        'DRBase.status',
+        'DRCamera.enable',
+        'DRCamera.hitResult',
+        'DRNavigateModule.arrive',
+        'DRNavigateModule.target',
+        'DRNavigateModule.targetState'
+    ]})
+        while True:
+            packet = d3.recv()
+            if packet != None:
+                event = packet['class'] + '.' + packet['key']
+                ''' if event == 'DRBase.status':
+                print(packet['data']) '''
+                if event == 'DRCamera.enable':
+                    print('camera enabled')
+                elif event == 'DRCamera.hitResult':
+                    print('camerahitResult = ---->', packet['data'], '<----')
+                elif event == 'DRNavigateModule.arrive':
+                    print('navigate arrive = ---->', packet['data'], '<----')
+                elif event == 'DRNavigateModule.target':
+                    print('navigate target = ---->', packet['data'], '<----')
+                elif event == 'DRNavigateModule.targetState':
+                    print('navigate target state  = ---->', packet['data'], '<----')    
+
+    except KeyboardInterrupt:
+        d3.close()
+        print('cleaned up')
+        sys.exit(0)
