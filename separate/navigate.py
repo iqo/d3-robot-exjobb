8cc170f8a5a41028d536c59c6dcb5a85628f83c9
iqo <niklas.brynolf@gmail.com>
2020-07-16 14:01:51 +0200

    comments

--- separate/navigate.py
+++ separate/navigate.py
@@ -8,8 +8,8 @@ def navigate():
         'DRCamera.hitResult',
         'DRNavigateModule.newTarget'
         ]})
-        d3.sendCommand('screensaver.nudge');
-        d3.sendCommand('camera.enable', { 'template': 'screen' });
+        #d3.sendCommand('screensaver.nudge');
+        #d3.sendCommand('camera.enable', { 'template': 'screen' });
         d3.sendCommand('navigate.enable');
         d3.sendCommand('navigate.obstacleAvoidance.setLevel',{'level' : '2'});
         #d3.sendCommand('navigate.target ', {'x':0,'y':0,'angleRadians':0,'relative':False,'dock':False,'dockId':0});
@@ -19,7 +19,7 @@ def navigate():
             if packet != None:
                 event = packet['class'] + '.' + packet['key']
                 if event == 'DRCamera.hitResult':
-                    d3.sendCommand('navigate.hitResult', {'hit': True,'xCamera': -0.2511, 'yCamera': -0.443, 'type': 'drivable', 'x': 4.186, 'y': 0.212, 'z': 0, 'angle': 0,'info1': '', 'info2': ''});
+                    d3.sendCommand('navigate.hitResult', {'hit': True,'xCamera': -0.2511, 'yCamera': -0.443, 'type': 'drivable', 'x': 5.6, 'y':3.321, 'z': 0, 'angle': 0,'info1': '', 'info2': ''});
                     # d3.sendCommand('navigate.target',packet['data'])
                     print('camerahitResult = ---->', packet['data'], '<----')
                 elif event == 'DRNavigateModule.newTarget':
@@ -28,7 +28,7 @@ def navigate():
         d3.close()
         d3.sendCommand('navigate.cancelTarget')
         d3.sendCommand('navigate.disable');
-        d3.sendCommand('camera.disable');
+        #d3.sendCommand('camera.disable');
         print('cleaned up')
         sys.exit(0)
 
