049cf243133d6badb4400a20b0affc73a98a67a9
iqo <niklas.brynolf@gmail.com>
2020-07-23 20:39:03 +0200

    corect files

--- separate/autoNavigateClass.py
+++ separate/autoNavigateClass.py
@@ -12,98 +12,70 @@ from logging.handlers import QueueHandler, QueueListener
 import sys
 import threading
 from python_logging_rabbitmq import RabbitMQHandlerOneWay
-import parsingTest
 from SDK import double
+#import parsingTest
 
-test = parsingTest.pars()
-#Loading config
-config = toml.load("config_widefind.toml")
 
-# Initialize logging
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.DEBUG)
+#testData = '{"host":"WFGATEWAY-3ABFF8D01EFF","message":"REPORT:42478B1A6B8CBA16,0.2.7,-19,207,3515,28,-30,10,4.10,-78.86,858027*6707","source":"03FF5C0A2BFA3A9B","time":"2020-07-22T13:29:05.838339518Z","type":"widefind_message"}'
+class pars():
+    def __init__(self):
+        #test = parsingTest.pars()
+        #Loading config
+        self.config = toml.load("config_widefind.toml")
 
-# out_handler = logging.StreamHandler(sys.stdout)
-# out_handler.setLevel(logging.DEBUG)
-# logger.addHandler(out_handler)
+        #MQTT IP for widefind
+        self.broker_url = self.config["connection"]["broker_ip"]
+        self.broker_port = self.config["connection"]["broker_port"]
 
-# rabbit_handler = RabbitMQHandlerOneWay(
-#    host=config['rabbitmq']['host'],
-#    username=config['rabbitmq']['username'],
-#    password=config['rabbitmq']['password'],
-#    connection_params={
-#       'virtual_host':'/',
-#       'connection_attempts': 3,
-#       'socket_timeout': 5000
-#    })
-# logger.addHandler(rabbit_handler)
+        self.blacklist = self.config["connection"]["blacklist"]#NO DATA EXPECTED FOR ADMIN THEREFORE IS IN BLACKLIST (no data downloaded for users in blacklist)
+        self.entrypoint = self.config["connection"]["entrypoint"]
+        self.port = self.config["connection"]["entrypoint_port"]
 
-#Creating Queue
-data_queue = queue.Queue()
+        #Creating Queue
+        self.data_queue = queue.Queue()
 
-#MQTT IP for widefind
-broker_url = config["connection"]["broker_ip"]
-broker_port = config["connection"]["broker_port"]
-
-blacklist = config["connection"]["blacklist"]#NO DATA EXPECTED FOR ADMIN THEREFORE IS IN BLACKLIST (no data downloaded for users in blacklist)
-entrypoint = config["connection"]["entrypoint"]
-port = config["connection"]["entrypoint_port"]
-
-def init_client():
-   client = mqtt.Client()
-   client.on_message = on_message
-   client.connect(broker_url, broker_port)
-   client.loop_start()
-   #client.subscribe("#")
-   client.subscribe("ltu-system/#")
-   #client.subscribe("wispr/anchor/#")
-   #logger.info("Initialized mqtt connection")
-
-def on_message(client, userdata, message):
-   mqttMsgString = message.payload.decode()
-   mqttMsgJson = json.loads(mqttMsgString)
-   #print(mqttMsgJson)
-   data_queue.put(mqttMsgJson)
+    def parsCordinates(self, data):
+        testParse = json.loads(data)
+        cords = testParse['message']
+        print(cords)
+        count = 0
+        for n in cords:
+            if n == ',':
+                count = 1 + count
+        split_string = cords.split(",", count)
+        xInMeter = (float(split_string[2])/1000)
+        yInMeter = (float(split_string[3])/1000)
+        zInMeter = (float(split_string[4])/1000)
+        return xInMeter, yInMeter, zInMeter
 
 
 
-# def send_data():
-#    parameters = pika.URLParameters(
-#       "amqp://"+config['rabbitmq']['username']+":"+config['rabbitmq']['password']+"@"+config['rabbitmq']['host']+"/"+config['rabbitmq']['username']
-#    )
-#    try:
-#       connection = pika.BlockingConnection(parameters)
-#       logger.info("Initialized RabbitMQ connection")
-#    except:
-#       logger.info("Failed to connect to RabbitMQ")
-#       raise
-#    channel = connection.channel()
-#    while True:
-#       message = data_queue.get(block=True)
-#       formated_message = message #{
-#          #"time": datetime.now().isoformat(),
-#          #"event_type": "data",
-#          #"sensor_type": "widefind",
-#          #"payload": message
-#       #} 
-#       #logger.debug("Sending message: {}".format(formated_message))
-#       message = json.dumps(formated_message)
-#       #print("BEACON" in message)
-#       if "BEACON" in message:
-#          channel.basic_publish(exchange=config['rabbitmq']['sensor_exchange'],
-#                               routing_key = config['rabbitmq']['routing_key'],
-#                               body = message
-#       )
-def testtest():
-   while True:
-      message = data_queue.get(block=True)
-      jsonMessage = json.dumps(message)
-      if "BEACON" in jsonMessage:
-      #print(jsonMessage)
-         test.parsCordinates(jsonMessage)
+    def on_message(self, client, userdata, message):
+        mqttMsgString = message.payload.decode()
+        mqttMsgJson = json.loads(mqttMsgString)
+        #print(mqttMsgJson)
+        self.data_queue.put(mqttMsgJson)
+
+    def init_client(self):
+        client = mqtt.Client()
+        client.on_message = self.on_message
+        client.connect(self.broker_url, self.broker_port)
+        client.loop_start()
+        client.subscribe("ltu-system/#")
+
+    def testtest(self):
+        while True:
+            message = self.data_queue.get(block=True)
+            jsonMessage = json.dumps(message)
+            if "BEACON" in jsonMessage:
+            #if "REPORT" in jsonMessage:
+                self.parsCordinates(jsonMessage)
+                #print(cord[0])
+
 if __name__ == '__main__':
-   init_client()
-   testtest()
+   test = pars()
+   test.init_client()
+   test.testtest()
    #send_data()
    #spawn thread mqtt
    #run send_data function
