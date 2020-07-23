5ee661810fac0f7a444023a097038326aa38915d
iqo <niklas.brynolf@gmail.com>
2020-07-23 21:32:18 +0200

    auto hit result test

--- separate/autoNavigateClass.py
+++ separate/autoNavigateClass.py
@@ -5,20 +5,22 @@ import time
 from datetime import datetime,timezone, timedelta, date
 import sys
 import json
-import pika
+#import pika
 import queue
 import logging
 from logging.handlers import QueueHandler, QueueListener
 import sys
 import threading
-from python_logging_rabbitmq import RabbitMQHandlerOneWay
+#from python_logging_rabbitmq import RabbitMQHandlerOneWay
 from SDK import double
-#import parsingTest
 
 
 #testData = '{"host":"WFGATEWAY-3ABFF8D01EFF","message":"REPORT:42478B1A6B8CBA16,0.2.7,-19,207,3515,28,-30,10,4.10,-78.86,858027*6707","source":"03FF5C0A2BFA3A9B","time":"2020-07-22T13:29:05.838339518Z","type":"widefind_message"}'
 class pars():
     def __init__(self):
+        #d3 sdk
+        self.d3 = double.DRDoubleSDK()
+
         #test = parsingTest.pars()
         #Loading config
         self.config = toml.load("config_widefind.toml")
@@ -63,19 +65,37 @@ class pars():
         client.loop_start()
         client.subscribe("ltu-system/#")
 
-    def testtest(self):
-        while True:
-            message = self.data_queue.get(block=True)
-            jsonMessage = json.dumps(message)
-            if "BEACON" in jsonMessage:
+    #def testtest(self):
+        #while True:
+            #message = self.data_queue.get(block=True)
+            #jsonMessage = json.dumps(message)
+            #if "BEACON" in jsonMessage:
             #if "REPORT" in jsonMessage:
-                self.parsCordinates(jsonMessage)
-                #print(cord[0])
+                #self.parsCordinates(jsonMessage)
+                #print(cord)
+
+    def navigateHitResult(self, xCamera= 0, yCamera = 0, zCordinate=0):
+        try:
+            self.d3.sendCommand('navigate.enable')
+            self.d3.sendCommand('navigate.obstacleAvoidance.setLevel',{'level' : '2'})
+            #self.d3.sendCommand('camera.hitTest', {'hit': 'true', 'x': 0.5,'y': 0.5,'z': 0, 'highlight': 'true'})
+            while True:
+                message = self.data_queue.get(block=True)
+                jsonMessage = json.dumps(message)
+                if "REPORT" in jsonMessage:
+                    cordinate = self.parsCordinates(jsonMessage)
+                    self.d3.sendCommand('navigate.hitResult', {'hit': True,'xCamera': float(xCamera), 'yCamera': float(yCamera), 'type': 'drivable', 'x': float(cordinate[0]), 'y':float(cordinate[1]), 'z': float(zCordinate), 'angle': 0,'info1': '', 'info2': ''})
+        except KeyboardInterrupt:
+            self.d3.close()
+            self.d3.sendCommand('navigate.cancelTarget')
+            #self.d3.sendCommand('navigate.disable')
+            print('cleaned up')
+            sys.exit(0)
 
 if __name__ == '__main__':
    test = pars()
    test.init_client()
-   test.testtest()
+   test.navigateHitResult()
    #send_data()
    #spawn thread mqtt
    #run send_data function
