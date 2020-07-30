55ee53bfb2f6eedf4228f0fe9ac342f864c91e77
iqo <niklas.brynolf@gmail.com>
2020-07-30 14:46:08 +0200

    ds

--- separate/audioCommand.py
+++ separate/audioCommand.py
@@ -46,8 +46,9 @@ while True:
     r = sr.Recognizer()
     m = sr.Microphone(device_index=29)
     with m as source:
+        recognizer.adjust_for_ambient_noise(source,duration=5)
         print("Say Something")
-        audio=r.listen(source,duration=5)
+        audio=r.listen(source)
     try:    
         print(r.recognize_google(audio),"\n")
     except sr.UnknownValueError:
