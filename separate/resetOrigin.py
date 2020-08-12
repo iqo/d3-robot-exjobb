ca2ecddac5bbd0de013c9f4cc28a973acb59e749
iqo <niklas.brynolf@gmail.com>
2020-08-12 14:46:49 +0200

    changed origion reset

--- separate/resetOrigin.py
+++ separate/resetOrigin.py
@@ -3,11 +3,11 @@ import sys
 
 def sendCommands():
     d3 = double.DRDoubleSDK()
-    try:
-        d3.sendCommand('pose.resetOrigin')
-    except KeyboardInterrupt:
-        d3.close()
-        print('Origin reset')
-        sys.exit(0)
+    d3.sendCommand('pose.resetOrigin')
+    d3.close()
+    print('Origin reset')
+    sys.exit(0)
+
+        
 
 sendCommands()
